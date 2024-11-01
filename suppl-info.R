f70f6a27d0a0cc53dd37f4d3201dc2257521f873
Kerri L. Coon <kerri.coon@wisc.edu>
2024-11-01 13:59:16 -0500

    Update suppl-info.R

--- suppl-info.R
+++ suppl-info.R
@@ -19,7 +19,8 @@ cor.test(prod$PercLarvFnd, prod$AvgLarvDens, method="spearman")
 prod2 <- read.csv("Hist-Master-Final.csv",header=TRUE)
 prod3 <- prod2[which(prod2$Posttreat==0&prod2$LarvFnd==1),]
 prod4 <- prod3[which(prod3$LarvDiv>0),]
-hist(prod4$LarvDiv)
+prod4$LarvDivPerDip <- prod4$LarvDiv/prod4$NumDips
+hist(prod4$LarvDivPerDip)
 
 ## Fig S2
 
